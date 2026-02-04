71b77d65555ab99afba6a133b1ff8749ad30c0c3
RagnorLorthbrk <ragnorlrthbrk@gmail.com>
2026-02-05 01:59:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("FurryFable blog automation initialized");
+import { google } from "googleapis";
+
+const auth = new google.auth.GoogleAuth({
+  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+});
+
+async function testSheetRead() {
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: "blogs!A1:F5"
+  });
+
+  console.log("Sheet data:", res.data.values);
+}
+
+testSheetRead();
