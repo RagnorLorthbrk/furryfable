36c517f253beb50f0dbcac3208578309f6563a8a
RagnorLorthbrk <ragnorlrthbrk@gmail.com>
2026-02-05 15:07:44 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ export const IMAGE_DIR = "images/blog";
 export const SHOPIFY = {
   storeDomain: process.env.SHOPIFY_STORE_DOMAIN,
   accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-  apiVersion: process.env.SHOPIFY_API_VERSION || "2026-01"
+  apiVersion: process.env.SHOPIFY_API_VERSION || "2026-01",
+  blogHandle: "blog"
 };
